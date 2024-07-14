4e479ddfce801c8406962a82c28e1609746e54f9
Samarth Kumar <samarthk2402@gmail.com>
2024-07-14 11:14:10 +0100

    Added project descriptions and animations

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
             I go to King Edwards VI Five Ways Grammar School and my favourite
             subjects are maths and computing. I enjoy programming a lot so I
             have built this website to show some of the projects I have made
-            using react.
+            using react. Click on the projects to see them in action!
           </p>
         </div>
 
@@ -22,11 +22,13 @@ function App() {
               imgSrc={"./wordleClone.png"}
               imgAlt={"Wordle clone screenshot"}
               projectSrc={"https://samarthk2402.github.io/WordleClone/"}
+              projectInfo={"This wordle clone was my first react project and it taught me a lot of the basic hooks such as useState and useEffect. I also played around with some css animations which I hadn't done before."}
             />
             <Project
               imgSrc={"./quoteGenerator.png"}
               imgAlt={"Quote Generator screenshot"}
               projectSrc={"https://samarthk2402.github.io/quote-generator/"}
+              projectInfo={"I made this quote generator as a very small project so I could better understand how to make API requests. It only took me a couple of hours to make but I feel I learnt a lot from it."}
             />
           </div>
         </div>
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,9 +1,12 @@
-const Project = ({ imgSrc, imgAlt, projectSrc }) => {
+const Project = ({ imgSrc, imgAlt, projectSrc, projectInfo }) => {
   return (
     <div className="project">
       <a href={projectSrc}>
         <img className="projectImage" src={imgSrc} alt={imgAlt} />
       </a>
+      <div className="projectInfo">
+        <p className="projectInfoText">{projectInfo}</p>
+      </div>
     </div>
   );
 };
